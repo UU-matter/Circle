6b5c8fec2971870a05cb6d1253f8396b7d622f29
unknown <student@G-226-1-2.tissu.local>
2015-02-25 12:50:12 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,37 @@
+/**
+ * Created by student on 25.02.2015.
+ */
+public class Circle {
+    private double radius;
+    private String color;
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public Circle() {
+        radius = 1.0;
+        color = "red";
+    }
+
+    public Circle(double r) {
+        radius = r;
+        color = "red";
+    }
+
+    public Circle(double r, String c) {
+        radius = r;
+        color = c;
+    }
+}
+
+
